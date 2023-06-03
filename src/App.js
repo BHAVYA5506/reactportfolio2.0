be780c89b5256bc71024885317baa8c76c29dc77
BHAVYA5506 <87484638+BHAVYA5506@users.noreply.github.com>
2023-06-03 12:21:46 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Navbar from "./components/navbar/Navbar";
+import './AppStyle.css'
+import Intro from '../src/components/intro/intro'
+
+function App() {
+  return (
+    <div className="App">
+     <Navbar/>
+     <Intro/>
+     <intro/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Services/services.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './services.css'
+import HeartEmoji from "../../img/heartemoji.png";
+import Glasses from "../../img/glasses.png";
+import Humble from "../../img/humble.png"
+const services = () => {
+  return (
+    <div className="services">
+      <div className="awesome">
+        <span>My Awesome</span>
+        <span>Services</span>
+        <span>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+        <br /> 
+         Illum, totam minus obcaecati veritatis nam odit.</span>
+      </div>
+      <div className="cards"></div>
+    </div>
+  );
+}
+
+export default services
